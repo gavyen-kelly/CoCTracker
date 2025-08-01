b731ada443659ee2750217b5f410cc9902f67dbe
gavyen <gavyen.r.kelly@gmail.com>
2025-08-01 15:39:00 -0500

    fixed api call for war log, prepend the %23 so that it works.

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,13 @@ app.get('/api/clans', async (req, res) => {
 app.get(`/api/:clanTag/warlog`), async (req, res) => {
     const { clanTag } = req.params
     try {
-        const response = await axios.get(`https://api.clashofclans.com/v1/clans/${clanTag}/warlog?limit=30`, {
+        const response = await axios.get(`https://api.clashofclans.com/v1/clans/%23${clanTag}/warlog?limit=30`, {
             headers: { Authorization: `Bearer ${process.env.CLASH_API_KEY}` }
         })
         fs.writeFileSync('server.log', JSON.stringify(response.data, null, 2) + '\n', { flag: 'a' });
         res.json(response.data);
     } catch (error) {
-        console.log('clan warlog error', JSON.stringify(response.data, null, 2))
+        console.error('Clan Details Error:', error.response?.data || error.message);
         fs.writeFileSync('server.log', JSON.stringify(error.response?.data || error.message, null, 2) + '\n', { flag: 'a' });
         res.status(500).json({ error: 'Failed to fetch clan war log data' });
     }
